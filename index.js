d065db78fe3fafc278cd72b252e99f8da92c4115
AyeSandarMin <ayesandarmin2018@gmail.com>
2024-01-21 19:38:06 +0700

    fix: sdfdsfdsfs

---
+++ index.js
@@ -0,0 +1,10 @@
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( const key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
+}
